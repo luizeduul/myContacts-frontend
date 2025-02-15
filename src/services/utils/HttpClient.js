aa814ae5c9c527c2997c216b72609a10ed1f5efd
Luiz Uliana <luizeduardr@gmail.com>
2025-02-14 22:42:12 -0300

    Ajuste http cliente

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -5,7 +5,20 @@ class HttpClient {
 
   async get(path) {
     const response = await fetch(`${this.baseUrl}/${path}`);
-    return response.json();
+
+    const contentType = response.headers.get('content-type');
+
+    let body = {};
+
+    if (contentType && contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(body?.error ?? `${response.status} - ${response.statusText}`);
   }
 }
 
