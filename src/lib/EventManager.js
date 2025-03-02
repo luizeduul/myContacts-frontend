d0ef5bc56159ee94af770774fe8b6ac5e96b57d4
Luiz Uliana <luizeduardr@gmail.com>
2025-03-02 14:21:20 -0300

    refatorando eventManager usando Map

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -1,36 +1,28 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (this.listeners[event]) {
-      this.listeners[event].forEach((listener) => listener(payload));
+    if (this.listeners.has(event)) {
+      this.listeners.get(event).forEach((listener) => listener(payload));
     }
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
     if (listeners) {
       const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove);
 
-      this.listeners[event] = filteredListeners;
+      this.listeners.set(event, filteredListeners);
     }
   }
 }
-
-const toastEventManager = new EventManager();
-
-toastEventManager.on('addtoast', () => {
-  console.log('listener2');
-});
-
-console.log(toastEventManager);
