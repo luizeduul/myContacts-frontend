3c3effe12f25e7016d04fafdfdc2a146670040f6
Luiz Uliana <luizeduardr@gmail.com>
2025-03-01 22:13:48 -0300

    iniciando toast

---
+++ src/components/Toast/ToastContainer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function ToastContainer() {
+  return (
+    <Container>
+      oi
+    </Container>
+  );
+}
+
+export default ToastContainer;
---
+++ src/components/Toast/ToastContainer/styles.js
@@ -0,0 +1,10 @@
+import styles from 'styled-components';
+
+export const Container = styles.div`
+  position: fixed;
+  bottom: 48px;
+  zindex: 2;
+  left: 50%;
+  background: red;
+  transform: translateX(-50%);
+`;
