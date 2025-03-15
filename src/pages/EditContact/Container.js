a9b759beba54fb0f4a53b9cd30170dd9929b73a3
Luiz Uliana <luizeduardr@gmail.com>
2025-03-15 13:57:53 -0300

    Refatorando editContact com containerPresentationPattern

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,75 @@
+import {
+  useCallback, useEffect, useRef,
+} from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import ContactsService from '../../services/ContactsService';
+import { toastError, toastSuccess } from '../../utils/toast';
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './Presentation';
+
+function Container() {
+  const params = useParams();
+
+  const { id } = params;
+
+  const history = useHistory();
+
+  const contactFormRef = useRef(null);
+
+  const [loading, setLoading] = useSafeAsyncState(true);
+  const [contactName, setContactName] = useSafeAsyncState('');
+
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldValues(contact);
+
+          setContactName(contact?.name);
+
+          setLoading(false);
+        });
+      } catch (error) {
+        safeAsyncAction(() => {
+          history.push('/');
+          toastError(error?.message || 'Ocorreu um erro ao carregar o contato');
+        });
+      }
+    }
+    loadContact();
+  }, [
+    history,
+    id,
+    setContactName,
+    setLoading,
+    safeAsyncAction,
+  ]);
+
+  const handleSubmit = useCallback(async (contact) => {
+    try {
+      const response = await ContactsService.updateContact(id, contact);
+
+      setContactName(response?.name);
+
+      toastSuccess('Contato editado com sucesso.');
+    } catch (error) {
+      toastError('Ocorreu um erro ao editar o contato');
+    }
+  }, [id, setContactName]);
+
+  return (
+    <Presentation
+      isLoading={loading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
+
+export default Container;
